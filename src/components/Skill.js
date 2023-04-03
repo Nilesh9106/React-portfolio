34d92b57ef39504ea0e990b237319393202d5f8c
Nilesh darji <nilesh.28.2003@gmail.com>
2023-04-03 17:17:41 +0530

    changes

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,48 +4,7 @@ import { IoLogoJavascript } from 'react-icons/io'
 import {SiPhp,SiDjango,SiPython,SiSpring} from 'react-icons/si'
 import cpp from './../img/cpp.png'
 
-const skills = [
-    {
-        icon:<FaReact/>,
-        title:"React JS",
-        color:"#61dafb",
-    },
-    {
-        icon:<FaJava/>,
-        title:"Java",
-        color:"#0088c7",
-    },
-    {
-        icon:<IoLogoJavascript/>,
-        title:"JavaScript",
-        color:"#ead41c",
-    },
-    {
-        icon:<img src={cpp}  className='h-12' alt="c++" />,
-        title:"C++",
-        color:"#005697",
-    },
-    {
-        icon:<SiPhp/>,
-        title:"PHP",
-        color:"#005697",
-    },
-    {
-        icon:<SiDjango/>,
-        title:"Django",
-        color:"#092e20",
-    },
-    {
-        icon:<SiPython/>,
-        title:"Python",
-        color:"#3877ad",
-    },
-    {
-        icon:<SiSpring/>,
-        title:"Spring",
-        color:"#67aa3c",
-    },
-]
+
 
 function Skill() {
     return (
@@ -55,20 +14,79 @@ function Skill() {
                 <div className="w-36 mx-auto h-1 bg-gray-200 rounded mt-3 mb-10"></div>
                 <div className=' px-5 mb-8'>
                     <div className="flex flex-wrap -m-4 text-center">
-                        {skills.map((data,index)=>{
-
-                            return(
-                                <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
-                                    <div className={`border-2 text-white hover:bg-[${data.color}] duration-500 border-gray-800 px-4 py-6 rounded-lg`}>
-                                        <span className='text-5xl m-3 flex justify-center'>
-                                            {data.icon}
-                                        </span>
-                                        <h2 className="title-font font-medium text-3xl text-white">{data.title}</h2>
-                                    </div>
-                                </div>
-                            )
-                        })}
                         
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#61dafb] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <FaReact />
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">React JS</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#ead41c] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <IoLogoJavascript />
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">Java Script</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#005697] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <img src={cpp}  className='h-12' alt="c++" />
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">C++</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#005697] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <SiPhp/>
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">PHP</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#092e20] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <SiDjango/>
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">Django</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#3877ad] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <SiPython/>
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">Python</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#0088c7] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <FaJava/>
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">Java</h2>
+                                
+                            </div>
+                        </div>
+                        <div className={"p-4 md:w-1/4 sm:w-1/2 hover:scale-105 duration-300 w-full"}>
+                            <div className={"border-2 text-white hover:bg-[#67aa3c] duration-500 border-gray-800 px-4 py-6 rounded-lg"}>
+                                <span className='text-5xl m-3 flex justify-center'>
+                                    <SiSpring/>
+                                </span>
+                                <h2 className="title-font font-medium text-3xl text-white">Spring</h2>
+                                
+                            </div>
+                        </div>
 
                         
                     </div>
